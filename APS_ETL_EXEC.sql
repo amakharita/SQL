ea51d698eb9da6d18bd6a714b9e8198b0ca878b7
Adam Makharita <amakharita88@gmail.com>
2017-05-08 20:23:49 -0400

    Update APS_ETL_EXEC.sql
    masked table and db names - example only

--- APS_ETL_EXEC.sql
+++ APS_ETL_EXEC.sql
@@ -2,7 +2,7 @@
 -- Begin APS data processing (premium) --
 -------------------------------------------------
 DECLARE @bat_status char(1) 
-DECLARE @num_co varchar(50); SET @num_co= '062'
+DECLARE @num_co varchar(50); SET @num_co= 'XXX
 DECLARE @cycle_name varchar(100)   
 DECLARE @d_begin datetime 
 DECLARE @d_end datetime 
@@ -11,7 +11,7 @@ DECLARE @d_inserted datetime
 DECLARE @id_bat int 
 DECLARE @send_emails         char(1) 
 
-SET @cycle_name = 'BAPU APS Processing'
+SET @cycle_name = 'XXX APS Processing'
 SET @send_emails = 'y' 
 
 EXEC sp_get_new_batch @num_co,@cycle_name,'p',@d_begin OUTPUT,@d_end OUTPUT,@id_bat OUTPUT
